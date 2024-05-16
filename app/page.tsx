ea3ea221c0cecff966d1c83117699f148a436b7d
Anukrit Jain <anukrit@Anukrits-MacBook-Air.local>
2024-05-17 04:14:39 +0530

    Added all components on the page

--- app/page.tsx
+++ app/page.tsx
@@ -3,29 +3,29 @@ import type { Metadata } from 'next';
 import { SITE } from '~/config.js';
 
 import Hero from '~/components/widgets/Hero';
-// import SocialProof from '../src/components/widgets/SocialProof';
+import SocialProof from '../src/components/widgets/SocialProof';
 import Features from '~/components/widgets/Features';
 import Content from '~/components/widgets/Content';
-// import Steps from '~/components/widgets/Steps';
-// import Testimonials from '~/components/widgets/Testimonials';
+import Steps from '~/components/widgets/Steps';
+import Testimonials from '~/components/widgets/Testimonials';
 import FAQs2 from '~/components/widgets/FAQs2';
-// import Pricing from '~/components/widgets/Pricing';
-// import Team from '~/components/widgets/Team';
-// import CallToAction2 from '~/components/widgets/CallToAction2';
-// import Contact from '~/components/widgets/Contact';
+import Pricing from '~/components/widgets/Pricing';
+import Team from '~/components/widgets/Team';
+import CallToAction2 from '~/components/widgets/CallToAction2';
+import Contact from '~/components/widgets/Contact';
 import {
-  // callToAction2Home,
-  // contactHome,
+  callToAction2Home,
+  contactHome,
   contentHomeOne,
   contentHomeTwo,
   faqs2Home,
   featuresHome,
   heroHome,
-  // pricingHome,
-  // socialProofHome,
-  // stepsHome,
-  // teamHome,
-  // testimonialsHome,
+  pricingHome,
+  socialProofHome,
+  stepsHome,
+  teamHome,
+  testimonialsHome,
 } from '~/shared/data/pages/home.data';
 
 export const metadata: Metadata = {
@@ -36,17 +36,17 @@ export default function Page() {
   return (
     <>
       <Hero {...heroHome} />
-      {/* <SocialProof {...socialProofHome} /> */}
+      <SocialProof {...socialProofHome} />
       <Features {...featuresHome} />
       <Content {...contentHomeOne} />
       <Content {...contentHomeTwo} />
-      {/* <Steps {...stepsHome} /> */}
-      {/* <Testimonials {...testimonialsHome} /> */}
+      <Steps {...stepsHome} />
+      <Testimonials {...testimonialsHome} />
       <FAQs2 {...faqs2Home} />
-      {/* <Pricing {...pricingHome} /> */}
-      {/* <Team {...teamHome} /> */}
-      {/* <Contact {...contactHome} /> */}
-      {/* <CallToAction2 {...callToAction2Home} /> */}
+      <Pricing {...pricingHome} />
+      <Team {...teamHome} />
+      <Contact {...contactHome} />
+      <CallToAction2 {...callToAction2Home} />
     </>
   );
 }
