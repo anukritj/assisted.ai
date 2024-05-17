24714ee7668fbb17a85303f90c57656381d2115f
Anukrit Jain <anukrit@Anukrits-MacBook-Air.local>
2024-05-17 17:31:27 +0530

    Added Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from 'next';
-
 import { SITE } from '~/config.js';
 
 import Providers from '~/components/atoms/Providers';
@@ -28,10 +27,34 @@ export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en" className={`motion-safe:scroll-smooth 2xl:text-[24px] ${customFont.variable} font-sans`}>
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),
+                    dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MSG53DCD');
+            `,
+          }}
+        />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className="tracking-tight antialiased text-gray-900 dark:text-slate-300">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MSG53DCD"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Providers>
           <Announcement />
           {/* <Header /> */}
